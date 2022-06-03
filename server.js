a48f7af50cfa2571f8f491b68c2b9e0ad1265983
Jared Arrington <jarrington00@yahoo.com>
2022-06-02 19:59:56 -0500

    working rapper api

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const rappers = {
+    '21 Savage':{
+        'age': 29,
+        'birthName': 'Sheyaa Bin Abraham-Joseph',
+        'birthLocation': 'London, England'
+    },
+    'Chance the Rapper':{
+        'age': 29,
+        'birthName': 'Chancelor Bennett',
+        'birthLocation': 'Chicago, Illinois'
+    },
+    'Unknonw':{
+        'age': 0,
+        'birthName': 'Unknown',
+        'bithLocation': 'Unknown'
+    }
+}
+
+app.get('/', (request, response)=>{
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:name', (request, response)=>{
+    const rapperName = request.params.name.toLowerCase()
+
+    if( rappers[rapperName] ){
+        response.json(rappers[rapperName])
+    }else{
+        response.json(rappers['unknown'])
+    }
+    
+})
+
+app.listen(PORT, ()=>{
+    console.log(`The server is now running on port ${PORT}!  Betta go catch it!`)
+})
+
